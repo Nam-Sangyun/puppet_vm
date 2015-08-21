ce78f050b5729e18610828c2e8147de3bdfc5c51
Nam Sangyun <kuria@linuxmaster.com>
2015-08-21 23:21:02 +0900

    Added file resource to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,12 @@ node 'kurias-cp.linuxmaster.com' {
 #   include sys_info
 }
 
+node 'vm01.linuxmaster.com' {
+    file {'/tmp/testserver':
+        content => "This is a test server.\n",
+}
+
 node /^vm[0-9]+\.linuxmaster\.com$/ {
-    include vim_upgrade
-    include sshd
+#    include vim_upgrade
+#    include sshd
 }
